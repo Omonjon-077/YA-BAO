0955981a9a81389045298c8951dc02f0a7a297be
Omonjon <omonjons077@gmail.com>
2022-07-04 12:51:57 +0500

    Two add pages commit

--- scripts/components/main.js
+++ scripts/components/main.js
@@ -1,18 +1,48 @@
 // Header Swiper //
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".headerSwiper", {
   slidesPerView: 1,
   spaceBetween: 30,
   slidesPerGroup: 1,
   grabCursor: true,
   loop: true
-}); // New Swiper //
+}); // Korzina Swiper //
+
+let swiperKor = new Swiper(".korzinaSwiper", {
+  slidesPerView: 'auto',
+  // spaceBetween: 10,
+  grabCursor: true,
+  loop: 'true'
+}); // Korzina Add //
+// let korzinaClass = document.getElementById('nav-korzina');
+// const korziAdd = document.getElementById('korzina');
+// const openModal = () => {
+//   korzinaClass.classList.add("korzi__active");
+// }
+// const closeModal = () => {
+//   korzinaClass.classList.remove("korzi__active");
+// }
+// korziAdd.addEventListener('click', () => {
+//   openModal()
+// })
+// New Swiper //
 
 let newSwiper = new Swiper(".new-swiper", {
-  // centeredSlides: true,
+  centeredSlides: true,
   slidesPerView: 'auto',
   spaceBetween: 16,
   grabCursor: true,
   loop: 'true'
+}); // Order Swiper //
+
+var orderSwiper = new Swiper(".orderSwip", {
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
 }); // Scroll Up //  
 
 function scrollUp() {
